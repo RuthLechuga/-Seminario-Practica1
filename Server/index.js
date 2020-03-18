0dd637238139f8fc16eba9e13a8fed89bffa8e31
Ubuntu <ubuntu@ip-172-31-18-216.us-east-2.compute.internal>
2020-03-18 04:11:26 +0000

    registro con foto version 1

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
-
 var app = express();
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
 let AWS = require('aws-sdk');
 
 app.use(bodyParser.json());
@@ -17,6 +19,20 @@ app.use(function (req, res, next) {
 
 AWS.config.loadFromPath('./config.json');
 var db = new AWS.DynamoDB.DocumentClient();
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
+
+const DIR = './uploads';
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, DIR);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+let upload = multer({ storage: storage });
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -73,19 +89,49 @@ app.post('/login', function(req,res) {
 
 });
 
-app.post('/register', function(req,res) {
+app.post('/register', upload.single('photo'), function(req,res) {
 
  const datos = req.body;
  const username = datos.username;
  const password = datos.password;
-// const url_photo = '';
+ let url_photo = "null";
+ console.log("username:",username);
+ console.log("password:",password);
+
+ if(req.file){
+	console.log(req.file.filename);
+
+	var uploadParams = { Bucket: 'bucketfotos10/usuarios', Key: '', Body: '' };
+    	var file = './uploads/' + req.file.filename;
+
+   	var fileStream = fs.createReadStream(file);
+    	fileStream.on("error", function (err) {
+      		console.log("File Error", err);
+    	});
+
+    	uploadParams.Body = fileStream;
+
+    	var path = require('path');
+    	uploadParams.Key = path.basename(file);
+
+    	s3.upload(uploadParams, function (err, data) {
+      		if (err) {
+        		console.log("Error", err);
+      		} if (data) {
+        		console.log("Upload Success", data.Location);
+			url_photo = data.Location;
+			console.log("x1",url_photo);
+      		}
+    	});
+ }
+ console.log("x2:",url_photo);
 
  let params = {
 	TableName: 'usuarios',
 	Item: {
 		'username': username,
-		'password': password
-		//'url_photo': url_photo
+		'password': password,
+		'url_photo': url_photo
 	}
  }
 
@@ -98,5 +144,4 @@ app.post('/register', function(req,res) {
 		res.send({register:true});
 	}
  });
-
 });
